f45a44a24a407289ca3a964bca9a7b921bc4796a
Florian Wöhrl <fw@woehrl.biz>
2026-01-08 14:43:58 +0100

    Author fixed

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
     "summary": "Use Odoo as an OAuth2 / OpenID Connect Identity Provider",
     "icon": "static/description/icon.png",
     "version": "18.0.1.0.0",
-    "author": "GJS Kanalkamera / PipeDesk",
+    "author": "Florian Wöhrl <fw@woehrl.biz>",
     "license": "LGPL-3",
     "category": "Authentication",
     "depends": ["base", "web", "auth_signup"],
